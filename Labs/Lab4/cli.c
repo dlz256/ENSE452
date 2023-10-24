34159d3595812dca64bd17729b78609ff149bedc
dlz256 <dlz256@uregina.ca>
2023-10-23 20:09:37 -0600

    Adding completed lab 4

--- Labs/Lab4/cli.c
+++ Labs/Lab4/cli.c
@@ -2,7 +2,7 @@
 	ENSE 452 Lab4
 	Dillan Zurowski
 	200431334
-	This lab contains an example of USART and TIM interrupts
+	This lab explores the ansi commands we can use to manipulate the terminal window
 
 */
 
@@ -14,11 +14,9 @@ char inputString[10] = ""; // Declare and initialize a character array to store 
 void CLI_Transmit(uint8_t *pData, uint16_t size) {
 	for (int  i=0; i<size; i++)
 	{
-			sendbyte(pData[i]);//if fail state(1)
+			sendbyte(pData[i]);		//loop through the array and send one byte at a time
 	}
 }
-
-
 
 
 //Receive data via USART
@@ -36,46 +34,55 @@ int CLI_Receive(uint8_t *input, uint16_t size) {
 		{            
 			if (strcmp(inputString, (const char *)"on") == 0)	//check if total string is a command
 			{
-				GPIOA->BSRR |= GPIO_BSRR_BS5;										//this function turns on an LED
-				const char* str = "\r\nThe LED is now on!"; 
-				uint16_t length = strlen(str);  
-				uint8_t* array = (uint8_t*)str;
-				CLI_Transmit(array, length);
+				GPIOA->BSRR |= GPIO_BSRR_BS5;		//this function turns on an LED
+				
+				const char LED_on_stmt[] = "\r\nThe LED is now on!"; 
+				CLI_Transmit(LED_on_stmt, sizeof(LED_on_stmt));
+				
+				CLI_Transmit(ANSI_SAVE_CURSOR, sizeof(ANSI_SAVE_CURSOR));						//save cursor position
+				CLI_Transmit(ANSI_MOVE_CURSOR_TOP, sizeof(ANSI_MOVE_CURSOR_TOP));		//move cursor to top
+				CLI_Transmit(ANSI_CLEAR_LINE, sizeof(ANSI_CLEAR_LINE));							//clear the top line
+				const char statement[] = "The LED is Turned ON";			
+				CLI_Transmit(statement, sizeof(statement));													//send message to top
+				CLI_Transmit(ANSI_SCROLL_REGION, sizeof(ANSI_SCROLL_REGION));
+				CLI_Transmit(ANSI_RESTORE_CURSOR, sizeof(ANSI_RESTORE_CURSOR));	//restore cursor to middle
 			}
 			else if (strcmp(inputString, (const char *)"off") == 0)
 			{
 				GPIOA->BSRR |= GPIO_BSRR_BR5;										//this function turns off the LED		
-				const char* str = "\r\nThe LED is now off!"; 
-				uint16_t length = strlen(str); 
-				uint8_t* array = (uint8_t*)str;
-				CLI_Transmit(array, length);
+				const char LED_off_stmt[] = "\r\nThe LED is now off!"; 
+				CLI_Transmit(LED_off_stmt, sizeof(LED_off_stmt));
+				
+				CLI_Transmit(ANSI_SAVE_CURSOR, sizeof(ANSI_SAVE_CURSOR));						//save cursor position
+				CLI_Transmit(ANSI_MOVE_CURSOR_TOP, sizeof(ANSI_MOVE_CURSOR_TOP));		//move cursor to top
+				CLI_Transmit(ANSI_CLEAR_LINE, sizeof(ANSI_CLEAR_LINE));							//clear the top line
+				const char statement[] = "The LED is Turned OFF";			
+				CLI_Transmit(statement, sizeof(statement));													//send message to top
+				CLI_Transmit(ANSI_SCROLL_REGION, sizeof(ANSI_SCROLL_REGION));				//reset scroll region
+				CLI_Transmit(ANSI_RESTORE_CURSOR, sizeof(ANSI_RESTORE_CURSOR));	//restore cursor to middle
+
 			}
 			else if (strcmp(inputString, (const char *)"help") == 0)
 			{
-				const char* str = "\r\n'on' to turn the LED on\r\n'off' to turn the LED off\r\n'Quit' to close connection"; 
-				uint16_t length = strlen(str);  
-				uint8_t* array = (uint8_t*)str;
-				CLI_Transmit(array, length);
+				const char help_stmt[] = "\r\n'on' to turn the LED on\r\n'off' to turn the LED off\r\n'Quit' to close connection"; 
+				CLI_Transmit(help_stmt, sizeof(help_stmt));
 			}
 			else if (strcmp(inputString, (const char *)"quit") == 0)
 			{
-				const char* str = "\r\nClosing serial connection";
-				uint16_t length = strlen(str);  //close serial connection and exit
-				uint8_t* array = (uint8_t*)str;
-				CLI_Transmit(array, length);
+				const char close_stmt[] = "\r\nClosing serial connection"; 
+				CLI_Transmit(close_stmt, sizeof(close_stmt));
 				serial_close();
 				return 1;
 			}
 			
+			//restarting prompt and erase inutString
 			memset(inputString, 0, sizeof(inputString));
-			const char* str = "\r\nEnter command(help for more info):"; 
-			uint16_t length = (uint16_t)strlen(str);
-			uint8_t* array = (uint8_t*)str;	
-			CLI_Transmit(array, length);
+			const char str[] = "\r\nEnter command(help for more info):"; 
+			CLI_Transmit(str, sizeof(str));
 
 			}	//end of enter case
 		
-		else 
+		else //if not enter or backspace, then add the letter to the saved inputString buffer. 
 			if (inputLength < sizeof(inputString) - 1) {
                 inputString[inputLength++] = input[0]; // Add the character to the inputString
                 inputString[inputLength] = '\0'; // Null-terminate the string
--- Labs/Lab4/main.c
+++ Labs/Lab4/main.c
@@ -2,12 +2,13 @@
 	ENSE 452 Lab4
 	Dillan Zurowski
 	200431334
-	This lab contains an example of USART and TIM interrupts
+	This lab explores the ansi commands we can use to manipulate the terminal window
 
 */
 
 #include "cli.h"
 #include <stdint.h>
+#include "main.h"
 
 extern int characterFlag;
 extern uint8_t characterReceived;
@@ -18,21 +19,31 @@ int main(void)
 {
 	serial_open();
 	
-	const char* str = "\r\nEnter command(help for more info):"; 
-	uint16_t length = (uint16_t)strlen(str);
-	uint8_t* array = (uint8_t*)str;	
-	CLI_Transmit(array, length);
+		CLI_Transmit(ANSI_SCROLL_REGION, sizeof(ANSI_SCROLL_REGION));	//set scroll window
+    CLI_Transmit(ANSI_CLEAR_SCREEN, sizeof(ANSI_CLEAR_SCREEN));		//clear the screen in case previous messages were printed
+    CLI_Transmit(ANSI_MOVE_CURSOR_TOP, sizeof(ANSI_MOVE_CURSOR_TOP));		//move the cursor to the top to set default message
 	
-	while(1)
-	{
-			if(characterFlag == 1)	//check if global flag is received
-			{
-				sendbyte(characterReceived);	//send received character
-				CLI_Receive(&characterReceived, 1);	//process character
-				characterFlag= 0;							//SET the flag back to 0
-			}
-				
-	}
+
+		const char statement[] = "The LED is Turned OFF";
+		CLI_Transmit(statement, sizeof(statement));						//display the status of the board, defaults to off
+	
+	
+    CLI_Transmit(ANSI_MOVE_CURSOR_MIDDLE, sizeof(ANSI_MOVE_CURSOR_MIDDLE));		//move the cursor to the scroll window in the middle
+		
+		const char welcomeMessage[] = "\r\nEnter command(help for more info):";		//start welcome message
+		CLI_Transmit(welcomeMessage, sizeof(welcomeMessage));
+		
+			
+		while(1)
+		{
+				if(characterFlag == 1)	//check if global flag is received
+				{
+					sendbyte(characterReceived);	//send received character
+					CLI_Receive(&characterReceived, 1);	//process character
+					characterFlag= 0;							//SET the flag back to 0
+				}
+					
+		}  
 	
 	return 0;
 }
