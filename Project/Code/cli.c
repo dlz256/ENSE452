532d4b001526839d8dc128130c75fcaa8245392e
dlz256 <dlz256@uregina.ca>
2023-11-30 17:25:27 -0600

    Final commit

--- Project/Code/cli.c
+++ Project/Code/cli.c
@@ -6,6 +6,9 @@
 	-Usart2 task to receive commands to control the direction, doors, and target floor
 	-Elevator Control task to iterate the floor number and open/close the door
 	-Emergencny stop/maintenance mode interrupt - triggered with the blue user button
+	Assumptions:
+	- Only one user at a time.
+	- The user starts on the current floor of the door
 */
 
 #include "cli.h"
@@ -16,9 +19,7 @@ QueueHandle_t directionQueue;
 QueueHandle_t floorQueue;
 QueueHandle_t arrivedQueue;
 QueueHandle_t EmergencyStopQueue;
-QueueHandle_t doorQueue;
-SemaphoreHandle_t floorSemaphore;
-
+//QueueHandle_t doorQueue;
 
 static char inputString[10] = ""; // Declare and initialize a character array to store the input string
 static int EmergencyFlag;
@@ -32,7 +33,12 @@ void CLI_Transmit(uint8_t *pData, uint16_t size) {
 }
 
 
-//Receive data via USART
+//Receive data via USART and process commands
+/*
+Commands:
+-'up' 'down': direction
+- '1-9': floor selection
+*/
 void CLI_Receive(uint8_t *input, uint16_t size) {
 		char direction;
 		int current_floor;
@@ -47,6 +53,10 @@ void CLI_Receive(uint8_t *input, uint16_t size) {
 		}		
 		else if (input[0] == 0x0D)	//if the enter button was clicked
 		{     
+			//up - send 'u' or 'd' to directionQueue
+			//select floor	- send to floorQueue
+			//end
+			
 		//if direction selected, then can check for floor queue
 		if (strcmp(inputString, (const char *)"up") == 0)
 		{
@@ -54,8 +64,9 @@ void CLI_Receive(uint8_t *input, uint16_t size) {
 			{
 				direction = 'u';
 				xQueueSendToFront(directionQueue, &direction, portMAX_DELAY);
-				const char str[] = "\r\nDoor closed!\r\nEnter a floor number(1-8):"; 
-				CLI_Transmit((uint8_t *)str, sizeof(str));	
+				const char str[] = "\r\n'Closing Door!\r\nEnter a floor number: "; 
+				CLI_Transmit((uint8_t *)str, sizeof(str));
+				
 			}
 		}
 		//down case, decrement
@@ -65,43 +76,48 @@ void CLI_Receive(uint8_t *input, uint16_t size) {
 			{
 				direction = 'd';
 				xQueueSendToFront(directionQueue, &direction, portMAX_DELAY);
-				const char str[] = "\r\nDoor closed!\r\nEnter a floor number(1-8):"; 
-				CLI_Transmit((uint8_t *)str, sizeof(str));				
+				const char str[] = "\r\n'Closing Door!\r\nEnter a floor number:"; 
+				CLI_Transmit((uint8_t *)str, sizeof(str));			
 			}						
 		}
-		else if (atoi(inputString) >= 1 && atoi(inputString) <= 8)
+		//number selected
+		else if (atoi(inputString) >= 1 && atoi(inputString) <= 9)
 		{
 			if( uxQueueMessagesWaiting( floorQueue ) == 0 )	//check if floor has already been selected
 			{				
 				int floor = atoi(inputString);
 				xQueueSendToFront(floorQueue, &floor, portMAX_DELAY);		
-				char statement[27];
-				snprintf(statement, sizeof(statement), "\r\nTravelling to floor: %d", floor);				
+				char statement[26];
+				snprintf(statement, sizeof(statement), "\r\nTravelling to floor: %d", floor);
+				
 				CLI_Transmit((uint8_t *)statement, sizeof(statement));			
-				
-				if (xSemaphoreTake(floorSemaphore, portMAX_DELAY) == pdTRUE) {
-				
-				if (current_floor == floor)	
+				xQueueReceive(floorQueue, &current_floor, portMAX_DELAY);		//receive confirmation that floor has been reached
+				if (current_floor ==floor)	
 				{
-					const char arrived_msg[] = "\r\nArrived! Opening door...\r\nEnter 'up' or 'down' to select direction"; 
-					CLI_Transmit((uint8_t *)arrived_msg, sizeof(arrived_msg));
+					const char str[] = "\r\nArrived! Opening Door...\r\nEnter Direction: "; 
+					CLI_Transmit((uint8_t *)str, sizeof(str));
 					int arrived = 0;
+					xQueueSendToFront(arrivedQueue, &arrived, portMAX_DELAY);		
+					CLI_Change_Floor_Number(current_floor);
+					
 				}
-			}
 			}						
-		}			
+		}	
+			
 			//restarting prompt and erase inutString
 			memset(inputString, 0, sizeof(inputString));
-			const char str[] = "\r\nEnter command:"; 
-			CLI_Transmit((uint8_t *)str, sizeof(str));
 		}	//end of enter case
+		//if not enter or backspace, then add the letter to the saved inputString buffer. 
 		else if (inputLength < sizeof(inputString) - 1) 
-		{//if not enter or backspace, then add the letter to the saved inputString buffer. 
+		{
 			inputString[inputLength++] = input[0]; // Add the character to the inputString
-			inputString[inputLength] = '\0'; // Null-terminate the string
+			inputString[inputLength] = '\0'; 			 // Null-terminate the string
     }
 } //end of receive function
 
+/*
+	This funciton is used to change the current floor number on the CLI
+*/
 void CLI_Change_Floor_Number (int floor_num)
 {
 		CLI_Transmit((uint8_t *)ANSI_SAVE_CURSOR, sizeof(ANSI_SAVE_CURSOR));						//save cursor position
@@ -116,6 +132,10 @@ void CLI_Change_Floor_Number (int floor_num)
 		CLI_Transmit((uint8_t *)ANSI_RESTORE_CURSOR, sizeof(ANSI_RESTORE_CURSOR));	//restore cursor to middle
 		
 }
+
+/*
+	This task outputs a message when the user presses the emergency button. 
+*/
 void Maintenance_Mode()
 {
 		CLI_Transmit((uint8_t *)ANSI_SAVE_CURSOR, sizeof(ANSI_SAVE_CURSOR));						//save cursor position
@@ -132,6 +152,7 @@ void Maintenance_Mode()
 	
 }
 
+//Interrupt for the blue user button on the STM32. This initiates emergency mode
 void EXTI15_10_IRQHandler(void){
 	if(EXTI->PR & EXTI_PR_PR13) { // Check if the interrupt is from line 13 (corresponding to PC13)
       EXTI->PR |= EXTI_PR_PR13; // Clear the pending bit for line 13
--- Project/Code/main.c
+++ Project/Code/main.c
@@ -6,12 +6,14 @@
 	-Usart2 task to receive commands to control the direction, doors, and target floor
 	-Elevator Control task to iterate the floor number and open/close the door
 	-Emergencny stop/maintenance mode interrupt - triggered with the blue user button
+	Assumptions:
+	- Only one user at a time.
+	- The user starts on the current floor of the door
 */
 
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
-
 #include "main.h"
 
 extern QueueHandle_t directionQueue;
@@ -19,8 +21,6 @@ extern QueueHandle_t CLIQueue;
 extern QueueHandle_t floorQueue;
 extern QueueHandle_t arrivedQueue;
 extern QueueHandle_t EmergencyStopQueue;
-static 	char direction[1] = "x";
-extern SemaphoreHandle_t floorSemaphore;
 
 int main(void)
 {	
@@ -30,33 +30,33 @@ int main(void)
 	floorQueue = xQueueCreate(1, sizeof(int));					//1-8
 	CLIQueue = xQueueCreate(1, sizeof(char));						//characters typed, from ISR
 	arrivedQueue = xQueueCreate(1, sizeof(int));				//confirmation of arrival
-	EmergencyStopQueue = xQueueCreate(1, sizeof(int));	//stop
+	EmergencyStopQueue = xQueueCreate(1, sizeof(int));	//confirmation of arrival
+
 
 	
 	xTaskCreate(vElevatorControlTask, "Elevator Controller", configMINIMAL_STACK_SIZE, NULL, mainELEVATOR_TASK_PRIORITY, NULL); 
 	xTaskCreate(vCLITask, "CLI", configMINIMAL_STACK_SIZE, NULL, mainCLI_TASK_PRIORITY, NULL); 
-	SemaphoreHandle_t floorSemaphore = xSemaphoreCreateBinary();
 
 	vTaskStartScheduler();
-while(1){}
+	while(1){}
 		
 }
-
+//This task controls the state of the elevator. 
+//Keeps track of the selected direction, current floor, and target floor. 
+/*
+	1. check for emergency stop button
+	2. receive direction 'u' or 'd' as well as desired floor number
+	3. iterate from current floor to the desired floor
+*/
 static void vElevatorControlTask(void * parameters)
 {
 	const int ELEVATOR_SPEED = 1500;	//changes floor every 1.5 seconds
 	int current_floor_number =1;
 	int selected_floor_number = 1;
 	bool at_floor = 0;
-	CLI_Change_Floor_Number(current_floor_number);
+	char direction[1] = "x";
 	bool maintenance_mode = 0;
 
-	/*
-	1. receive direction 'u' or 'd'
-	2. receive desired floor number
-	3. receive start message 's'
-	4. iterate, check for emergency stop button
-	*/
 	for (;;)
 	{
 		if( uxQueueMessagesWaiting( EmergencyStopQueue ) != 0  )	//check if direction has already been selected
@@ -66,12 +66,12 @@ static void vElevatorControlTask(void * parameters)
 			{
 				Maintenance_Mode();	//dislay maintenance mode on screen
 				xQueueReceive(EmergencyStopQueue, &maintenance_mode, portMAX_DELAY);		//block until the button is hit again.
+				xQueueSendToFront(floorQueue, &current_floor_number, portMAX_DELAY); 		//tell CLI the floor it is on
+
 				maintenance_mode = false;
-				current_floor_number = 1;
-				selected_floor_number = 1;
-				direction[0] = 'x';
-				xQueueSendToFront(floorQueue, &current_floor_number, portMAX_DELAY); //send message to CLI that it is starting over
 				CLI_Change_Floor_Number(current_floor_number);	//reset display. 
+				char welcomeMessage[] = "\r\nRestarting...\r\n'up'/'down', floor#'s 1-8\r\nEnter direction: ";		//start welcome message
+				CLI_Transmit((uint8_t *)welcomeMessage, sizeof(welcomeMessage));
 			}
 			vTaskDelay(10);
 		}
@@ -79,15 +79,17 @@ static void vElevatorControlTask(void * parameters)
 		{
 			xQueueReceive(directionQueue, &direction, portMAX_DELAY);		//idle state, waiting for "up" or "down" command
 			xQueueReceive(floorQueue, &selected_floor_number, portMAX_DELAY);		//in elevator, waiting for floor number
+
 		}
 		else if (strcmp(direction, (const char *)"u") == 0 && at_floor == false && maintenance_mode == false)
 			{
-					CLI_Change_Floor_Number(current_floor_number);			//change display
-					vTaskDelay(pdMS_TO_TICKS(ELEVATOR_SPEED));					//delay 1.5 seonds per floor
-					if (current_floor_number >= selected_floor_number)	//if reached selected floor, end
+					CLI_Change_Floor_Number(current_floor_number);
+					vTaskDelay((pdMS_TO_TICKS(ELEVATOR_SPEED)));
+					if (current_floor_number >= selected_floor_number)
 					{
 						at_floor = true;
-						xSemaphoreGive(floorSemaphore);
+						xQueueSendToFront(floorQueue, &current_floor_number, portMAX_DELAY); //send message to CLI that it has arrived
+						xQueueReceive(arrivedQueue, &at_floor, portMAX_DELAY);		//sets at_floor back to false
 
 						direction[0] = 'x';
 					}
@@ -112,22 +114,21 @@ static void vElevatorControlTask(void * parameters)
 	}
 }
 	
+//This task will control the input of the user and output feedback to the CLI
 static void vCLITask(void * parameters)
 {
 	
-		CLI_Transmit((uint8_t *)ANSI_SCROLL_REGION, sizeof(ANSI_SCROLL_REGION));	//set scroll window
-    CLI_Transmit((uint8_t *)ANSI_CLEAR_SCREEN, sizeof(ANSI_CLEAR_SCREEN));		//clear the screen in case previous messages were printed
-    CLI_Transmit((uint8_t *)ANSI_MOVE_CURSOR_TOP, sizeof(ANSI_MOVE_CURSOR_TOP));		//move the cursor to the top to set default message
+	CLI_Transmit((uint8_t *)ANSI_SCROLL_REGION, sizeof(ANSI_SCROLL_REGION));	//set scroll window
+	CLI_Transmit((uint8_t *)ANSI_CLEAR_SCREEN, sizeof(ANSI_CLEAR_SCREEN));		//clear the screen in case previous messages were printed
+	CLI_Transmit((uint8_t *)ANSI_MOVE_CURSOR_TOP, sizeof(ANSI_MOVE_CURSOR_TOP));		//move the cursor to the top to set default message
 
-		char floorStartMessage[] = "Floor 1:";		//start welcome message
-		CLI_Transmit((uint8_t *)floorStartMessage, sizeof(floorStartMessage));
-		CLI_Transmit((uint8_t *)ANSI_MOVE_CURSOR_MIDDLE, sizeof(ANSI_MOVE_CURSOR_MIDDLE));		//move the cursor to the scroll window in the middle
-	
-		char welcomeMessage[] = "\r\n'up'/'down', 'open'/'close', floor#'s 1-8\r\nEnter command:";		//start welcome message
-		CLI_Transmit((uint8_t *)welcomeMessage, sizeof(welcomeMessage));
-	
+	char floorStartMessage[] = "Floor: 1";		//start welcome message
+	CLI_Transmit((uint8_t *)floorStartMessage, sizeof(floorStartMessage));
+	CLI_Transmit((uint8_t *)ANSI_MOVE_CURSOR_MIDDLE, sizeof(ANSI_MOVE_CURSOR_MIDDLE));		//move the cursor to the scroll window in the middle
 
-		
+	char welcomeMessage[] = "\r\n'up'/'down', floor#'s 1-8\r\nEnter direction: ";		//start welcome message
+	CLI_Transmit((uint8_t *)welcomeMessage, sizeof(welcomeMessage));
+
 	uint8_t characterReceived;
 	for (;;)
 	{
@@ -137,7 +138,6 @@ static void vCLITask(void * parameters)
 			sendbyte(characterReceived);				//send received character
 			CLI_Receive(&characterReceived, 1);	//process character
 		}		
-		vTaskDelay(10);
 	}
 
 }
